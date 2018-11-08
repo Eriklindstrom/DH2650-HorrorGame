48118ee1d6e45e8a1a85c47a4f70fdf840a9d53c
Bjarni <bjarniragnarg@gmail.com>
2018-11-08 11:18:00 +0100

    Added House controller, including light dimmer

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants : MonoBehaviour
+{
+    public static string TAG_LIGHTS = "ElectricLight";
+}
---
+++ Assets/Scripts/HouseController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseController : MonoBehaviour {
+
+    /******************\
+        Configurables
+    \******************/
+
+    [SerializeField] float madness;
+    [SerializeField] float madnessThreshold = 10;
+
+    /**********************\
+        Non-configurable
+    \**********************/
+
+    [HideInInspector] public float madnessPercentage = 0.0f;
+    private KeyValuePair<Light, float>[] electricLights;
+
+    /***************\
+        Functions
+    \***************/
+
+    void Start ()
+    {
+        //Madness is zero to begin with        
+        madness = 0.0f;
+
+        //Initialize list of lights, also store their initial intensities        
+        GameObject[] lightObjects = GameObject.FindGameObjectsWithTag(GameConstants.TAG_LIGHTS);
+        electricLights = new KeyValuePair<Light, float>[lightObjects.Length];
+        for (int i = 0; i < lightObjects.Length; i++)
+        {
+            Light currentLight = lightObjects[i].GetComponent<Light>();
+            electricLights[i] = new KeyValuePair<Light, float>(currentLight, currentLight.intensity);
+        }
+    }
+	
+	void Update ()
+    {
+        madness += Time.deltaTime;
+        madnessPercentage = madness / madnessThreshold;
+
+        Horrify();
+	}
+
+    void Horrify()
+    {
+        DimLights();
+        
+        //TODO add sounds       
+    }
+
+    void DimLights()
+    {
+        for(int i = 0; i < electricLights.Length; i++)
+        {
+            Light currentLight = electricLights[i].Key;
+            float initialIntensity = electricLights[i].Value;
+            currentLight.intensity = initialIntensity - (initialIntensity * madnessPercentage);
+        }
+    }
+
+    void FlickerCandles()
+    {
+
+    }
+}
