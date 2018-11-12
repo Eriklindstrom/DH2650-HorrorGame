a664ed4ee402d02929e31689157f7f307cea39e1
Bjarni <bjarniragnarg@gmail.com>
2018-11-12 16:59:17 +0100

    Add sound to falling book

---
+++ Assets/Scripts/BookController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookController : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<AudioSource>().Play();
+        Debug.Log("collision");
+    }
+}
