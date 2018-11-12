63e3e6c983a8ee748770489e0eb8e597e6526952
vincent <inzywong@gmail.com>
2018-11-12 17:10:40 +0100

    model Second bedroom

---
+++ Assets/Scripts/rotateBlade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateBlade : MonoBehaviour
+{
+
+    [SerializeField] private float speedMultiplier = 40f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        transform.Rotate(Vector3.forward * Time.deltaTime * speedMultiplier);
+
+    }
+}
