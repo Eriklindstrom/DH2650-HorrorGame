67b0aca4dd47a5a6af75e9373ed4fa3ec4a4f00b
Bjarni <bjarniragnarg@gmail.com>
2018-11-28 15:01:10 +0100

    Players get sanity for putting books back on shelf

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class BookController : MonoBehaviour {
 
+    [SerializeField] private bool mainBook = false;
+
+    [SerializeField] private GameObject bookGameObj;
+
+    private BookshelfPuzzle puzzleController;
+
+    void Start()
+    {
+        puzzleController = bookGameObj.GetComponent<BookshelfPuzzle>();
+    }
+
     void OnCollisionEnter(Collision other)
     {
         GetComponent<AudioSource>().Play();
+
+        if(other.gameObject.tag == "Floor" && !puzzleController.IsPuzzleStarted())
+        {
+            puzzleController.StartPuzzle();
+        }
     }
 
     void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/BookshelfPuzzle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookshelfPuzzle : MonoBehaviour {
+
+    [SerializeField] private int rewardPerBookReturned = 10;
+    [SerializeField] private GameObject houseControllerObj;
+
+    private HouseController houseController;
+
+    private int currentNumberOfBooks;
+    private int allTimeMaxNumberOfBooks;
+    private bool puzzleStarted = false;
+
+	void Start ()
+    {
+        currentNumberOfBooks = 0;
+        allTimeMaxNumberOfBooks = 0;
+
+        houseController = houseControllerObj.GetComponent<HouseController>();
+	}
+	
+	void Update ()
+    {
+        if (!puzzleStarted) return;
+
+		if(currentNumberOfBooks > allTimeMaxNumberOfBooks)
+        {
+            houseController.madness -= rewardPerBookReturned;
+            allTimeMaxNumberOfBooks = currentNumberOfBooks;
+        }
+	}
+
+    public void StartPuzzle()
+    {
+        puzzleStarted = true;
+        allTimeMaxNumberOfBooks = currentNumberOfBooks;
+    }
+
+    public bool IsPuzzleStarted()
+    {
+        return puzzleStarted;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Book")
+        {
+            currentNumberOfBooks++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Book")
+        {
+            currentNumberOfBooks--;
+        }
+    }
+}
