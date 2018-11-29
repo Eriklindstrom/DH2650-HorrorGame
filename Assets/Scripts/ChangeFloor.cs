325832d0c0f490b6946a262868dc7b7123b745dd
VIC <>
2018-11-29 17:00:33 +0100

    start of changing scene work

--- Assets/Scripts/ChangeFloor.cs
+++ Assets/Scripts/ChangeFloor.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeFloor : MonoBehaviour {
+public class ChangeFloorUp : MonoBehaviour {
 
-    [SerializeField] private GameObject goDownCollider;
-    [SerializeField] private Animation anim;
+    [SerializeField] private GameObject goUpCollider;
+    //[SerializeField] private Animation anim;
 
-    void OnTriggerEnter(Collider ChangeScene) // can be Collider HardDick if you want.. I'm not judging you
+    public float pX;
+    public float pY;
+    public float pZ;
+
+    // Use this for initialization
+    void Start()
     {
-        anim.Play();        //Not set up
-        SceneManager.LoadScene(1);
-            //Application.LoadLevelAdditive(1); //1 is the build order it could be 1065 for you if you have that many scenes
+        // Check if the values have been saved
+        if (PlayerPrefs.GetInt("Saved") == 1)
+        {
+            pX = PlayerPrefs.GetFloat("p_x");
+            pY = PlayerPrefs.GetFloat("p_y");
+            pZ = PlayerPrefs.GetFloat("p_z");
+
+            transform.position = new Vector3(pX, pY, pZ);
+
+            // Reset, so that the save will be used only once
+            PlayerPrefs.SetInt("Saved", 0);
+            PlayerPrefs.Save();
+        }
     }
+
+    void position_save()
+    {
+        PlayerPrefs.SetFloat("p_x", transform.position.x);
+        PlayerPrefs.SetFloat("p_y", transform.position.y);
+        PlayerPrefs.SetFloat("p_z", transform.position.z);
+
+        PlayerPrefs.SetInt("Saved", 1);
+        // You need to actually save the values!
+        PlayerPrefs.Save();
+    }
+    void position_load()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    void OnTriggerEnter(Collider ChangeScene) 
+    {
+        //anim.Play();        //Not set up
+        position_load();
+        //SceneManager.LoadScene(0);
+    }
+
 }
