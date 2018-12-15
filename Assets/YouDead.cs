411308d0951a14fb3890085a10ff7437e7c78ede
VIC <>
2018-12-15 16:11:18 +0100

    minor tweaks

---
+++ Assets/YouDead.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouDead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Name " + col.transform.name);
+        if (col.transform.tag == "MainCamera")
+        {
+            Debug.Log("test");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
